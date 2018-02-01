c1e13ca6ae662319fec1535f2f41983f9f2675fe
Sven Kroell <mail@svenkroell.de>
2018-02-01 11:44:28 +0000

    add oartial

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require("express")
-const app = express()
-const port = process.env.PORT
+const express = require("express");
+const express_handlebars = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', express_handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+const port = process.env.PORT;
 
 app.get('/', (request, response) => {
-    response.send(`Hello`)
-})
+    const title = "Welcome";
+    response.render("index", {
+        title: title
+    });
+});
 
 app.get('/about', (request, response) => {
-    response.send("Aboasdut")
-})
+    response.render("about");
+});
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`)
-})
+    console.log(`Server started on ${port}`);
+});
