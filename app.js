05a18d89f9d25c2dd7f4dcc0eb01f9e1d3e3d5ae
Sven Kroell <sven.kroell@gmail.com>
2018-03-28 13:39:56 +0200

    12factorise app

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+const databaseUsername = process.env.DB_USERNAME || "nody";
+const databasePassword = process.env.DB_PASSWORD || "unicorntest";
+const expressPort = process.env.PORT || 5000;
+
+mongoose.connect(`mongodb://${databaseUsername}:${databaseUsername}@ds225308.mlab.com:25308/vidjod`).then(
+    () => console.log("Connected to mongodb")
+).catch(
+    (error) => console.log(error)
+);
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -23,8 +33,7 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-const port = 5000;
 
-app.listen(port, () =>{
-  console.log(`Server started on port ${port}`);
+app.listen(expressPort, () =>{
+  console.log(`Server started on port ${expressPort}`);
 });
