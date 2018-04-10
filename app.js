dbafd7b2bcf792c4c3e2e28691b221a000d384b4
Sven Kroell <sven.kroell@gmail.com>
2018-04-10 14:41:33 +0200

    move app

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http')
+const path = require('path');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -10,17 +12,21 @@ const app = express();
 const databaseUsername = process.env.DB_USERNAME || "nody";
 const databasePassword = process.env.DB_PASSWORD || "unicorntest";
 const expressPort = process.env.PORT || 5000;
+const viewsDir = 'app/views'
 
 mongoose.connect(`mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`).then(
-    () => console.log("Connected to mongodb")
+    () => console.log('Connected to mongodb')
 ).catch(
     (error) => console.log(error)
 );
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
+  defaultLayout: 'main',
+  layoutsDir: `${__dirname}/${viewsDir}/layouts`,
+  partialsDir: `${viewsDir}/partials`
 }));
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/app/views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
