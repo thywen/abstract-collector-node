bd17d2e3d6aa2eba8feb85bb1c438c856bf5f088
Sven Kroell <sven.kroell@gmail.com>
2018-04-11 16:58:43 +0200

    extract database

--- app.js
+++ app.js
@@ -7,19 +7,11 @@ const mongoose = require('mongoose');
 const middlewares = require('./app/middlewares')
 const configureViews = require('./app/views');
 const routes = require('./app/routes')
+const db = require('./app/database')
 
 const app = express();
 
-const databaseUsername = process.env.DB_USERNAME || "nody";
-const databasePassword = process.env.DB_PASSWORD || "unicorntest";
 const expressPort = process.env.PORT || 5000;
-
-
-mongoose.connect(`mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`).then(
-  () => console.log('Connected to mongodb')
-).catch(
-  (error) => console.log(error)
-);
 
 app.use(middlewares)
 app.use(routes)
--- tests/homeControllerTest.js
+++ tests/homeControllerTest.js
@@ -4,19 +4,36 @@ var homeController = require('../app/controllers/homeController')
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
-const server = require('../app')
+var server
+var request
 
 chai.use(chaiHttp);
 
-const agent = chai.request.agent(server);
-const request = chai.request(server);
+
+beforeEach( () => {
+  server = require('../app')
+  request = chai.request(server);
+})
+
+
 
 describe("Routes", function () {
   describe("GET Users", function () {
 
-    it("should respond", function (done) {
+    it("should open the main page", function (done) {
       request.get('/').then((res) => {
         expect(res.text).to.have.string('Jot down ideas for your next abstracts')
+        done();
+      })
+      .catch(function (err) {
+        throw err;
+        done();
+      });
+    })
+
+    it("should open the about page", function (done) {
+      request.get('/about').then((res) => {
+        expect(res.text).to.have.string('This is a Node/Express app for jotting down abstracts')
         done();
       })
       .catch(function (err) {
