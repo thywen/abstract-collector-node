a4c36e889b2597d7935be9a5e93464c128409d02
Sven Kroell <sven.kroell@gmail.com>
2018-04-12 15:51:31 +0200

    add abstract comntroller tests

--- tests/routes/abstractsControllerTest.js
+++ tests/routes/abstractsControllerTest.js
@@ -1,13 +1,54 @@
-var sinon = require('sinon');
-var chai = require('chai');
-var proxyquire = require('proxyquire');
-var supertest = require('supertest');
-var express = require('express');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiHttp = require('chai-http');
+var server
+var request
 
-var mongoose = require('../mocks/mongoose_mock');
+chai.use(chaiHttp);
+
+
+beforeEach(() => {
+    server = require('../../app')
+    request = chai.request(server);
+})
+
 
 describe('AbstractValidator', () => {
-    it('should work', () => {
-            
+    it("should inform that details are missing", function (done) {
+        request.post('/abstracts')
+            .send({ title: 'bla' })
+            .end(function (err, res) {
+                expect(res).to.have.status(200)
+                expect(res.text).to.match(/Please add details/)
+                if (err) return done(err);
+                done();
+            });
+    })
+})
+
+describe('AbstractValidator', () => {
+    it("should open the main page", function (done) {
+        request.post('/abstracts')
+            .send({ details: 'bla' })
+            .end(function (err, res) {
+                expect(res).to.have.status(200)
+                expect(res.text).to.match(/Please add a title/)
+                if (err) return done(err);
+                done();
+            });
+    })
+})
+
+describe('AbstractValidator', () => {
+    it("should inform that everything is missing", function (done) {
+        request.post('/abstracts')
+            .send({})
+            .end(function (err, res) {
+                expect(res).to.have.status(200)
+                expect(res.text).to.match(/Please add details/)
+                expect(res.text).to.match(/Please add a title/)
+                if (err) return done(err);
+                done();
+            });
     })
 })
