c28e17570a495130d2b12c42918345125a632388
Sven Kroell <sven.kroell@gmail.com>
2018-04-12 16:48:13 +0200

    refactor naming

--- tests/routes/homeControllerTest.js
+++ tests/routes/homeControllerTest.js
@@ -10,45 +10,43 @@ var request
 chai.use(chaiHttp);
 
 
-beforeEach( () => {
+beforeEach(() => {
   server = require('../../app')
   request = chai.request(server);
 })
 
 
 
-describe("Routes", function () {
-  describe("GET Users", function () {
+describe("Static Routes", function () {
 
-    it("should open the main page", function (done) {
-      request.get('/').then((res) => {
-        expect(res.text).to.have.string('Jot down ideas for your next abstracts')
-        expect(res).to.have.status(200)
-        done();
-      })
+  it("should open the main page", function (done) {
+    request.get('/').then((res) => {
+      expect(res.text).to.have.string('Jot down ideas for your next abstracts')
+      expect(res).to.have.status(200)
+      done();
+    })
       .catch(function (err) {
         throw err;
         done();
       });
-    })
+  })
 
-    it("should open the about page", function (done) {
-      request.get('/about').then((res) => {
-        expect(res.text).to.have.string('This is a Node/Express app for jotting down abstracts')
-        expect(res).to.have.status(200)
-        done();
-      })
+  it("should open the about page", function (done) {
+    request.get('/about').then((res) => {
+      expect(res.text).to.have.string('This is a Node/Express app for jotting down abstracts')
+      expect(res).to.have.status(200)
+      done();
+    })
       .catch(function (err) {
         throw err;
         done();
       });
-    })
+  })
 
-    it('should render a 404 page when an unknown page is opened', (done) => {
-      request.get('/blablabla').then((res) => {
-        expect(res).to.have.status(404)
-        done();
-      })
+  it('should render a 404 page when an unknown page is opened', (done) => {
+    request.get('/blablabla').then((res) => {
+      expect(res).to.have.status(404)
+      done();
     })
   })
 })
