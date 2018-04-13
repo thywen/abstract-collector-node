41c8f786e43ba0daf8d82bc4a8e853f44a37eaa5
Sven Kroell <sven.kroell@gmail.com>
2018-04-13 12:40:42 +0200

    add flash messages

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 
 router.use(require('./bodyParser'));
 router.use(require('./methodOverride'))
+router.use(require('./expressSession'))
+router.use(require('./connectFlash'))
 
 module.exports = router;
