e5d36d2892527cbabc98b491de1b88f4ecdb708e
Sven Kroell <sven.kroell@gmail.com>
2018-04-13 13:40:27 +0200

    remove passwords

--- app/database.js
+++ app/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const databaseUsername = process.env.DB_USERNAME || "nody";
-const databasePassword = process.env.DB_PASSWORD || "unicorntest";
+const databaseUsername = process.env.DB_USERNAME
+const databasePassword = process.env.DB_PASSWORD
 const localDB = `mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`
 const database = process.env.MONGODB_URI || localDB
 const test = "tst"
 
 const db = mongoose.connect(database).then(
-    () => console.log('Connected to mongodb' + `on ${database}`)
+    () => console.log('Connected to mongodb')
 ).catch(
     (error) => console.log(error)
 );
