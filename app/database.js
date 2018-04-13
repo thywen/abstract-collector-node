0d7614ac1142b5b33ccf66c0351f310f445c6099
Sven Kroell <sven.kroell@gmail.com>
2018-04-13 13:34:49 +0200

    add database uri

--- app/database.js
+++ app/database.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const databaseUsername = process.env.DB_USERNAME || "nody";
 const databasePassword = process.env.DB_PASSWORD || "unicorntest";
+const localDB = `mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`
+const database = process.env.MONGODB_URI || localDB
 
-const db = mongoose.connect(`mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`).then(
+const db = mongoose.connect(database).then(
     () => console.log('Connected to mongodb')
 ).catch(
     (error) => console.log(error)
