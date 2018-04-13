b250f0b72914e7ab3c0cbbc4864f74cee870e01b
Sven Kroell <sven.kroell@gmail.com>
2018-04-13 13:38:06 +0200

    test

--- app/database.js
+++ app/database.js
@@ -4,6 +4,7 @@ const databaseUsername = process.env.DB_USERNAME || "nody";
 const databasePassword = process.env.DB_PASSWORD || "unicorntest";
 const localDB = `mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`
 const database = process.env.MONGODB_URI || localDB
+const test = "tst"
 
 const db = mongoose.connect(database).then(
     () => console.log('Connected to mongodb' + `on ${database}`)
