5d9b370a4e6f092ef8b9d3cf5a9bcfa5b6dbff52
Sven Kroell <sven.kroell@gmail.com>
2018-04-13 13:48:24 +0200

    rework users

--- app/database.js
+++ app/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const databaseUsername = process.env.DB_USERNAME
+const databaseUsername = process.env.DB_USERNAME || "nody"
 const databasePassword = process.env.DB_PASSWORD
 const localDB = `mongodb://${databaseUsername}:${databasePassword}@ds225308.mlab.com:25308/vidjod`
 const database = process.env.MONGODB_URI || localDB
 const test = "tst"
 
 const db = mongoose.connect(database).then(
-    () => console.log('Connected to mongodb')
+    () => console.log('Connected to mongodb on ' + database)
 ).catch(
     (error) => console.log(error)
 );
