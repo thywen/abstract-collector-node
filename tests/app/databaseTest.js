f9698841600382cbf06b0579983082b3f62fa5a1
Sven Kroell <sven.kroell@gmail.com>
2018-04-17 13:02:03 +0200

    improve test converage

--- tests/app/databaseTest.js
+++ tests/app/databaseTest.js
@@ -1,23 +1,45 @@
 const mongoose = require('mongoose');
 var connection, server;
-
 const expect = require('chai').expect;
 
 describe('Database Connection', () => {
 
-    beforeEach(() => {
-        process.env.PROD = true;
-        connection = mongoose.connection;
-        server = require('../../app');
+    describe('Connected', () => {
+        beforeEach(() => {
+            process.env.PROD = true;
+            connection = mongoose.connection;
+            connection.host = null;
+            server = require('../../app');
+        });
+
+        it('connects to the test database', (done) => {
+            expect(connection.host).to.not.be.null;
+            done()
+        });
+
+        afterEach((done) => {
+            server.close();
+            connection.close()
+            done();
+        });
     });
-    
-    afterEach(() => {
-        server.close();
-        connection.close();
-    });
-    
-    it('connects to the test database', (done) => {
-        expect(connection.host).to.not.be.null;
-        done();
+
+    describe('Not Connected', () => {
+
+        beforeEach(() => {
+            process.env.PROD = false;
+            mongoose.connection.host = null;
+            server = require('../../app');
+        });
+
+
+        it('does not connect to the test database', (done) => {
+            expect(connection.host).to.be.null;
+            done();
+        });
+
+        afterEach(() => {
+            server.close();
+        });
     });
 });
